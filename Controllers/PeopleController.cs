22ae0e8f37a8f6815cd5b61bffd2700f738d9262
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-15 22:47:02 +0200

    Create a method and views to create and list of people

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,34 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "David", LastName = "Mortin", Age = 30 });
+            people.Add(new PersonModel { FirstName = "John", LastName = "Doe", Age = 35 });
+            people.Add(new PersonModel { FirstName = "Sarah", LastName = "Andreson", Age = 28 });
+
+            return View(people);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
